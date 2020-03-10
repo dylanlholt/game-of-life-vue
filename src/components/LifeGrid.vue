f25e31a8bc17d2d22ef2efd3effc9c7f2d443c66
Dylan Holt <dylan.lee.holt@gmail.com>
2020-03-09 23:26:35 -0400

    Added function for updating main array to next gen

--- src/components/LifeGrid.vue
+++ src/components/LifeGrid.vue
@@ -1,23 +1,24 @@
 <template>
-  <!-- <div>{{mainArr.length}}</div> -->
-  <svg id="grid" height=85vh width=85vh>  
-    <g v-for="(row,rowIndex) in mainArr" :key="rowIndex">
-      <rect 
-        v-for="(col,colIndex) in row" :key="colIndex"
-        :x=10*colIndex
-        :y=10*rowIndex
-        :width=10
-        :height=10
-        stroke=black
-        :fill="[col<0.25 ? 'black' : 'white']">
-      </rect>
-    </g>
-  </svg>
+  <div>
+    <button v-on:click='nextGen'>Next Generation</button>
+    <svg id="grid" height=85vh width=85vh>  
+      <g v-for="(row,rowIndex) in mainArr" :key="rowIndex">
+        <rect 
+          v-for="(col,colIndex) in row" :key="colIndex"
+          :x=10*colIndex
+          :y=10*rowIndex
+          :width=10
+          :height=10
+          stroke=black
+          :fill="[col<0.25 ? 'black' : 'white']">
+        </rect>
+      </g>
+    </svg>
+  </div>
 </template>
   
 <script>
 export default {
-  el: "#grid",
   data: function() {
     return {
       mainArr: this.generateData(50,50)
@@ -36,7 +37,18 @@ export default {
       return outArr
     },
     nextGen(){
-
+      let newArr = this.mainArr.map((d,i)=>{
+        if (i==0){
+          return d.map(x=>x*2)
+        }
+        else if (i==this.mainArr.length-1){
+          return d.map(x=>x*2)
+        }
+        else {
+          return d.map(x=>x*2)
+        }
+      })
+      this.mainArr = newArr
     }
   }
 }
